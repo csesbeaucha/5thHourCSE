dae8e157067c2df1df600676fb430fe1f310ff2e
sarahwidder <sarahwidder@claytonschools.net>
2015-11-10 12:02:14 -0600

    added algorithm 1 in place of player 10
    collude until partner betrays, then betray until partner colludes, then
    collude again

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -370,16 +370,16 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 10:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'algorithm 1'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
-            else:
-                return 'c' #otherwise collude
+            elif opponent_history[-1]=='b':
+                return 'b' # betray if partner betrayed the last round
+            elif opponent_history[-1]=='c':
+                return 'c' #collude if partner colluded the last round
 
 
 
