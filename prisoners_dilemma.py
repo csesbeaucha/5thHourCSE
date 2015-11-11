7e0c245af5765c7f12832d3db7224548b0cb6842
RoninCenturion <joshuaglover@claytonschools.net>
2015-11-11 12:10:17 -0600

    Final code for collaboration of class

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -443,7 +443,7 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
             if len(opponent_history)==0: #It's the first round: collude
                 return 'c'
             elif history[-1]=='c' and opponent_history[-1]=='c':
-                return 'c' # betray to gain if they collude again
+                return 'b' # betray to gain if they collude again
             else:
                 return 'c' #otherwise collude
     
