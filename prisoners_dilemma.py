947f938db14d378cb4dc2922d7714c47fbc25e63
csesbeaucha <stephenbeauchamp@claytonschools.net>
2015-11-12 12:27:28 -0600

    rrr

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -1,9 +1,6 @@
 from __future__ import print_function
 
-<<<<<<< HEAD
-''' Justin Guilak
-=======
-''' Sarah Widder
+'''
 >>>>>>> refs/remotes/origin/master
 PrisonerDilemma.py allows hard-coding different strategies
 for the Iterative Prisoners Dilemma, the canonical game of game-theory.
