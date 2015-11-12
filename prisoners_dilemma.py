766185e18d1823dcf63b549a1e4cdce7dcc2c320
IsaiahHayward <isaiahhayward@claytonschools.net>
2015-11-12 11:49:38 -0600

    recoded player 14

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -460,19 +460,14 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 14:
         if getting_team_name:
-            return 'loyal vengeful occasionally greedy'
+            return 'Isaiah'
         else:
-            if len(opponent_history)==0: #It's the first round: collude
+            if len(opponent_history)==0:
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were severely punished last time
+            elif opponent_history[-1]=='b':
+                return 'b'
             else:
-                if random.random()<0.1: #10% of the other rounds
-                    return 'b'         #betray
-                else:
-                    return 'c'         #otherwise collude
-    
-    
+                return 'c'
     
     
 
